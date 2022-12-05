0464d580c9457aa9647c31a5dd14d0531f023214
l3miage-konatemo <97477836+l3miage-konatemo@users.noreply.github.com>
2022-12-05 15:37:17 +0100

    Update EntrepriseTest.java

--- src/test/java/com/ipi/jva350/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/EntrepriseTest.java
@@ -1,63 +1,18 @@
 package com.ipi.jva350;
 
 import com.ipi.jva350.model.Entreprise;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import org.assertj.core.api.Assertions;
-=======
 import com.ipi.jva350.model.SalarieAideADomicile;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
->>>>>>> 16ddc81bef366e2dab8592145c08f495fb9d1b96
-=======
-import com.ipi.jva350.model.SalarieAideADomicile;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
->>>>>>> f6bf26f4343bd6e328710cc849b6b21ab2dd46d8
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-public class EntrepriseTest {
-    @ParameterizedTest(name = "test est dans plage")
-    @CsvSource({
-            "'2022-01-01','2021-06-01','2022-06-01', true",
-            "'2022-06-01','2022-06-01','2022-12-01', false",
-            "'2022-10-15','2021-06-01','2022-06-01', false",
-    })
-    public void estDansPlage(String d, String dateDebut, String dateFin, boolean expected){
-        //Given
-        Entreprise entreprise=new Entreprise();
-        //When
-        boolean verif=entreprise.estDansPlage(LocalDate.parse(d),LocalDate.parse(dateDebut), LocalDate.parse(dateFin));
-        //Then
-        Assertions.assertThat(verif).isEqualTo(expected);
-
-    }
-
-    @ParameterizedTest(name = "test estJourFerie")
-    @CsvSource({
-            "'2022-01-01', true",
-            "'2022-06-01', false",
-            "'2022-12-25', true",
-            "'2022-05-01', true"
-    })
-    public void estJourFerie(String jour,boolean expected){
-        //Given
-        Entreprise entreprise=new Entreprise();
-        //When
-        boolean verif=entreprise.estJourFerie(LocalDate.parse(jour));
-=======
-=======
->>>>>>> f6bf26f4343bd6e328710cc849b6b21ab2dd46d8
 import java.util.LinkedHashSet;
 
 public class EntrepriseTest {
 
-    @ParameterizedTest(name = "test estDansPlage")
+    @ParameterizedTest(name = "TestestDansPlage")
     @CsvSource({
             "'2022-12-01', '2022-10-01', '2022-11-14', false",
             "'2022-07-01', '2021-05-01', '2021-12-25', false",
@@ -70,33 +25,11 @@ public class EntrepriseTest {
         Entreprise entreprise= new Entreprise();
         //When
         boolean verif=Entreprise.estDansPlage(LocalDate.parse(d),LocalDate.parse(dateDebut),LocalDate.parse(dateFin));
-<<<<<<< HEAD
->>>>>>> 16ddc81bef366e2dab8592145c08f495fb9d1b96
-=======
->>>>>>> f6bf26f4343bd6e328710cc849b6b21ab2dd46d8
         //Then
         Assertions.assertThat(verif).isEqualTo(expected);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @ParameterizedTest(name = "test estJourFerie")
-    @CsvSource({
-            "'2022-01-01', true",
-            "'2022-06-01', false",
-            "'2022-12-25', true",
-            "'2022-05-01', true"
-    })
-    public void proportionPondereeDuMois(String MoisDuConge){
-
-    }
-
-
-
-=======
-=======
->>>>>>> f6bf26f4343bd6e328710cc849b6b21ab2dd46d8
-    @ParameterizedTest(name = "test estDansPlage")
+    @ParameterizedTest(name = "TestEstJourFerie")
     @CsvSource({
             "'2022-12-01',  false",
             "'2022-07-01', false",
@@ -113,7 +46,7 @@ public class EntrepriseTest {
         Assertions.assertThat(verif).isEqualTo(expected);
     }
 
-    @ParameterizedTest(name = "test estDansPlage")
+    @ParameterizedTest(name = "TestProportionPonderee")
     @CsvSource({
             "'2022-12-01',  16",
             "'2022-07-01', 28",
@@ -128,7 +61,8 @@ public class EntrepriseTest {
         //Then
         Assertions.assertThat(verif).isEqualTo(expected);
     }
-    @ParameterizedTest(name = "test estDansPlage")
+
+    @ParameterizedTest(name = "TestGetPremierJourAnnéeConge")
     @CsvSource({
             "'2022-12-01',  '2022-06-01'",
             "'2022-07-01', '2022-06-01'",
@@ -144,22 +78,5 @@ public class EntrepriseTest {
         //Then
         Assertions.assertThat(verif).isEqualTo(LocalDate.parse(expected));
     }
-<<<<<<< HEAD
->>>>>>> 16ddc81bef366e2dab8592145c08f495fb9d1b96
-=======
-    @ParameterizedTest(name = "test estJourFerie")
-    @CsvSource({
-            "'2022-01-01', true",
-            "'2022-06-01', false",
-            "'2022-12-25', true",
-            "'2022-05-01', true"
-    })
-    public void proportionPondereeDuMois(String MoisDuConge){
 
-    }
-
-
-
-
->>>>>>> f6bf26f4343bd6e328710cc849b6b21ab2dd46d8
 }
